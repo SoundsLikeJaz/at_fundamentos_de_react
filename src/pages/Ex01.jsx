cb2bfc4f968cf34ba16d1280074664ee53573e8f
Jackson Silva <jacksonslv36@gmail.com>
2024-06-17 14:59:11 -0300

    Update Ex01.jsx
    Apaguei os comentários

--- src/pages/Ex01.jsx
+++ src/pages/Ex01.jsx
@@ -2,21 +2,6 @@ import { faker } from "@faker-js/faker";
 import DataTable from "react-data-table-component";
 
 const Ex01 = () => {
-    
-    // const columns = [
-    //     {
-    //         name: "Nome",
-    //         selector: row => row.nome
-    //     },
-    //     {
-    //         name: "Descrição",
-    //         selector: row => row.descricao
-    //     },
-    //     {
-    //         name: "Preço",
-    //         selector: row => row.preco
-    //     }
-    // ];
 
     const produtos = new Array(10).fill().map((value) => {
         let produto = new Object();
